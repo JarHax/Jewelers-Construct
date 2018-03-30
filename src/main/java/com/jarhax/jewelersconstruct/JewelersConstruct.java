44f33549ce123685a031abe9cad0dfc921ef6bfd
Tyler Hancock <darklime@live.ca>
2018-03-30 16:44:12 -0600

    Added registries.

--- src/main/java/com/jarhax/jewelersconstruct/JewelersConstruct.java
+++ src/main/java/com/jarhax/jewelersconstruct/JewelersConstruct.java
@@ -1,17 +1,35 @@
 package com.jarhax.jewelersconstruct;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.jarhax.jewelersconstruct.api.Material;
+import com.jarhax.jewelersconstruct.api.Modifier;
+
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.RegistryBuilder;
 
 @Mod(modid = "jewelersconstruct", name = "Jewelers Construct", version = "@VERSION@")
+@EventBusSubscriber
 public class JewelersConstruct {
+    
+    public static final Logger LOG = LogManager.getLogger("Jewelers Construct");
     
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
         
+        LOG.info("Creating registries!");
+        createForgeRegistry("modifiers", Modifier.class);
+        createForgeRegistry("materials", Material.class);
+        LOG.info("Registries created!");
     }
     
     @EventHandler
@@ -22,5 +40,10 @@ public class JewelersConstruct {
     @EventHandler
     public void postInit (FMLPostInitializationEvent event) {
         
+    }
+    
+    private static <T extends IForgeRegistryEntry<T>> IForgeRegistry<T> createForgeRegistry (String name, Class<T> type) {
+        
+        return new RegistryBuilder<T>().setName(new ResourceLocation("jewelersconstruct", name)).setType(type).setMaxID(Integer.MAX_VALUE >> 5).create();
     }
 }
