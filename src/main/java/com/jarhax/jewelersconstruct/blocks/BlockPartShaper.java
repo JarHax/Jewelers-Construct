4fb979aee46126b6c837c99ce36d03e4f8ec9ecd
Jared <jaredlll08@gmail.com>
2018-03-31 02:56:16 +0200

    added the tile

--- src/main/java/com/jarhax/jewelersconstruct/blocks/BlockPartShaper.java
+++ src/main/java/com/jarhax/jewelersconstruct/blocks/BlockPartShaper.java
@@ -1,12 +1,15 @@
 package com.jarhax.jewelersconstruct.blocks;
 
+import com.jarhax.jewelersconstruct.tileentities.TileEntityPartShaper;
+import javax.annotation.Nullable;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockChest;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockPartShaper extends Block {
     
@@ -18,5 +21,16 @@ public class BlockPartShaper extends Block {
     @Override
     public AxisAlignedBB getBoundingBox (IBlockState state, IBlockAccess source, BlockPos pos) {
         return new AxisAlignedBB(0,0,0,1,1-0.0625,1);
+    }
+    
+    @Override
+    public boolean hasTileEntity (IBlockState state) {
+        return true;
+    }
+    
+    @Nullable
+    @Override
+    public TileEntity createTileEntity (World world, IBlockState state) {
+        return new TileEntityPartShaper();
     }
 }
---
+++ src/main/java/com/jarhax/jewelersconstruct/tileentities/TileEntityPartShaper.java
@@ -0,0 +1,22 @@
+package com.jarhax.jewelersconstruct.tileentities;
+
+import net.darkhax.bookshelf.block.tileentity.TileEntityBasicTickable;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class TileEntityPartShaper extends TileEntityBasicTickable {
+    
+    @Override
+    public void onEntityUpdate () {
+    
+    }
+    
+    @Override
+    public void writeNBT (NBTTagCompound dataTag) {
+    
+    }
+    
+    @Override
+    public void readNBT (NBTTagCompound dataTag) {
+    
+    }
+}
