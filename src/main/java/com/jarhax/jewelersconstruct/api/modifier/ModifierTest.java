dd8eb8a8ee221644e999b8a19f50d8b1d0786ccb
Tyler Hancock <darklime@live.ca>
2018-03-30 21:52:16 -0600

    Updated the modifier test to use attributes and durability.

--- src/main/java/com/jarhax/jewelersconstruct/api/modifier/ModifierTest.java
+++ src/main/java/com/jarhax/jewelersconstruct/api/modifier/ModifierTest.java
@@ -1,18 +1,34 @@
 package com.jarhax.jewelersconstruct.api.modifier;
 
+import java.util.UUID;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import net.darkhax.bookshelf.data.AttributeOperation;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.ItemStack;
 
 public class ModifierTest extends Modifier {
     
+    public static final AttributeModifier BUFF_TEST = new AttributeModifier(UUID.fromString("88f2f87f-c94e-4d1b-8bd2-2e686f26d50b"), "jewelersconstruct_modifier_debug", 100, AttributeOperation.ADDITIVE.ordinal());
     public ModifierTest() {
         
         this.setRegistryName("test");
     }
     
     @Override
-    public void onWearerTick (ItemStack item, EntityLivingBase user, int level) {
+    public Multimap<String, AttributeModifier> getAttributeModifiers (ItemStack stack, EntityLivingBase wearer, int level) {
         
-        System.out.println("Test");
+        final Multimap<String, AttributeModifier> map = HashMultimap.create();
+        map.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), BUFF_TEST);
+        return map;
+    }
+    
+    public int getModifiedDurability (ItemStack item, int base) {
+        
+        return 100;
     }
 }
